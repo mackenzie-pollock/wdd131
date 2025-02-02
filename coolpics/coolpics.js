92a597c27b94167ca5aaa93f627bc9136dea4852
mackenzie-pollock <pollockmackenzie312@gmail.com>
2025-02-02 00:11:51 -0700

    coolpics pt 2 1/2 done

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -5,5 +5,28 @@ function myFunction() {
     } else {
       x.style.display = "none";
     }
-  }
+}
   
+document.getElementById("menu-button").addEventListener("click", function () {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");
+});
+
+function handleResize() {
+  const menu = document.getElementById("menu");
+  if(window.innerWidth > 1000) {
+    menu.classList.remove("hidden");
+  } else{
+    menu.classList.add("hidden");
+  }
+}
+    
+window.addEventListener("resize", handleResize);
+
+handleResize();
+
+document.getElementById("menu-button").addEventListener("click", function () {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
+});
