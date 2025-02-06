97fda0ddf7f7dc4edf8a2d5634d5221b07f0ebed
mackenzie-pollock <pollockmackenzie312@gmail.com>
2025-02-06 11:45:24 -0700

    fixed nav bar toggle

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -10,7 +10,11 @@ function viewerTemplate(imgSrc, altText) {
   let imgSrc = "images/norris-full.jpeg"; // Replace with actual path or URL
   let altText = "full image"; 
 
-  
+  document.getElementById("menu-button").addEventListener("click", function () {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("hidden");  // Toggles visibility
+});
+
 
 function viewHandler(event) {
   const clickedImg = event.target;
